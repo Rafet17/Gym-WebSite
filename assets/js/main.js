d4bc4ab6f9fa8e414835c22a730107303d95cd1d
Rafet17 <rafet.hasanov17@gmail.com>
2024-08-12 12:48:07 +0400

    Header section and header's responsive design for tablets and phones

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// JavaScript kodu
+document.querySelector('.button i').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+});
