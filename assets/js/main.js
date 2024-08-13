b0fdf047f3145e9d6d40d01c40938088d27a21e1
Rafet17 <rafet.hasanov17@gmail.com>
2024-08-13 22:28:17 +0400

    Testimonials section, resposive design and swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,11 @@ document.querySelector('.button i').addEventListener('click', function() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 });
+
+
+const swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
